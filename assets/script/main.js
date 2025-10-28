dbbd1bedf4bb57a68eac473a68112ba71725c551
Kasper <polinalevcenko55@gmail.com>
2025-10-28 16:56:44 +0700

    анимации + структирование отзывов

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,6 +1,9 @@
 const triggers = document.querySelectorAll('.how-we-work__scroll p');
 const slides = document.querySelectorAll('.how-we-work__slide');
 const images = document.querySelectorAll('.img_emoji img');
+const reviewsContainer = document.querySelector('.reviews__inner'); 
+const cards = document.querySelectorAll('.reviews__card');
+const reviewsBtn = document.querySelector('.reviews__button')
 
 triggers.forEach(trigger => {
 trigger.addEventListener('click', () => {
@@ -31,3 +34,40 @@ images.forEach(img => {
         img.src = origSrc;
     });
 });
+
+
+const cardsArray = [...cards];
+    
+function currentPositions() {
+    return cardsArray.map(card => { 
+        const styleCSS = window.getComputedStyle(card);
+        return {
+            top: styleCSS.top, 
+            left: styleCSS.left,
+            right: styleCSS.right,
+            bottom: styleCSS.bottom,
+            zIndex: styleCSS.zIndex
+        };
+    });
+}   
+
+let positions = currentPositions();
+    
+reviewsContainer.addEventListener('click', (e) => { 
+    if (!e.target.closest('.reviews__card')) return; 
+
+    for (let i = 0; i < cards.length; i++) { 
+        const nextIndex = (i + 1) % 4; 
+        cards[i].style.top = positions[nextIndex].top;
+        cards[i].style.left = positions[nextIndex].left;
+        cards[i].style.right = positions[nextIndex].right;
+        cards[i].style.bottom = positions[nextIndex].bottom;
+        cards[i].style.zIndex = positions[nextIndex].zIndex;
+    }
+    positions = currentPositions();
+});
+
+reviewsBtn.addEventListener('click', () => { 
+    reviewsContainer.classList.replace("reviews__inner", "structured")
+})
+
